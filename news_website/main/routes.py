38f8ce9339dd30305286f1e96cb53a3e88c326d7
Dhruv <dhruvsp.inexture@gmail.com>
2022-06-13 11:37:11 +0530

    remove unused code

--- news_website/main/routes.py
+++ news_website/main/routes.py
@@ -3,12 +3,6 @@ from flask.views import MethodView
 
 main = Blueprint("main", __name__)
 
-
-# @main.route('/')
-# @main.route('/home')
-# def home_page():
-#     return render_template('home.html')
-
 class homePage(MethodView):
     """Class for getting the home page for the website"""
 
