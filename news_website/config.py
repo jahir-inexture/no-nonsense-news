0dc13d5a0e4654a640cef3cb673800b6470c8591
Dhruv <dhruvsp.inexture@gmail.com>
2022-07-04 16:14:42 +0530

    implemented cloudinary

--- news_website/config.py
+++ news_website/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 class Config:
     """class which stores important configuration information required for different usages"""
+
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
